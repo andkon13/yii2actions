c9c84dcbfcac94835a44d44d44f529fb0190198d
akonenkov <akonenkov@cnstr.ru>
2014-08-27 11:08:12 +0400

    add table prefix to migrate

--- Migration.php
+++ Migration.php
@@ -7,35 +7,56 @@
  */
 
 namespace andkon\yii2actions;
-
+/**
+ * Class Migration
+ * Надстройка над миграциями
+ *
+ * @package andkon\yii2actions
+ */
 class Migration extends \yii\db\Migration
 {
+    protected $tablesPrefix = '';
     protected $tables = [];
     protected $table = null;
     protected $foreignKeys = [];
     protected $fields = [];
     protected $vals = [];
 
+    /**
+     * переопределение safeUp
+     *
+     * @return void
+     */
     public function safeUp()
     {
-        $this->_upTables();
-        $this->_upFields();
-        $this->_upKeys();
-        $this->_upVals();
+        $this->upTables();
+        $this->upFields();
+        $this->upKeys();
+        $this->upVals();
 
         parent::safeUp();
     }
 
+    /**
+     * переопределение safeDown
+     *
+     * @return void
+     */
     public function safeDown()
     {
         parent::safeDown();
 
-        $this->_downKeys();
-        $this->_downFields();
-        $this->_downTable();
+        $this->downKeys();
+        $this->downFields();
+        $this->downTable();
     }
 
-    private function _upTables()
+    /**
+     * Создание таблиц
+     *
+     * @return bool
+     */
+    private function upTables()
     {
         if (empty($this->tables)) {
             return true;
@@ -43,37 +64,47 @@ class Migration extends \yii\db\Migration
 
         $options = 'ENGINE=InnoDB DEFAULT CHARACTER SET=utf8 COLLATE=utf8_general_ci';
         foreach ($this->tables as $table => $fields) {
-            $this->createTable($table, $fields, $options);
+            $this->createTable($this->tablesPrefix . $table, $fields, $options);
         }
 
         return true;
     }
 
-    private function _upFields()
+    /**
+     * добавление полей
+     *
+     * @return bool
+     */
+    private function upFields()
     {
         if (empty($this->fields) || $this->table == null) {
             return true;
         }
 
         foreach ($this->fields as $field => $type) {
-            $this->addColumn($this->table, $field, $type);
+            $this->addColumn($this->tablesPrefix . $this->table, $field, $type);
         }
 
         return true;
     }
 
-    private function _upKeys()
+    /**
+     * создание внешних ключей
+     *
+     * @return bool
+     */
+    private function upKeys()
     {
         if (empty($this->foreignKeys)) {
             return true;
         }
 
         foreach ($this->foreignKeys as $key) {
-            $name = $this->_getFkName($key);
+            $name = $this->getFkName($key);
             $key  = array_merge(['delete' => 'CASCADE', 'update' => 'NO ACTION'], $key);
             $this->addForeignKey(
-                $name, $key['from'][0], $key['from'][1],
-                $key['to'][0], $key['to'][1],
+                $name, $this->tablesPrefix . $key['from'][0], $key['from'][1],
+                $this->tablesPrefix . $key['to'][0], $key['to'][1],
                 $key['delete'], $key['update']
             );
         }
@@ -81,7 +112,12 @@ class Migration extends \yii\db\Migration
         return true;
     }
 
-    private function _upVals()
+    /**
+     * добавление записей в тиблицы
+     *
+     * @return bool
+     */
+    private function upVals()
     {
         if (empty($this->vals)) {
             return true;
@@ -89,55 +125,77 @@ class Migration extends \yii\db\Migration
 
         foreach ($this->vals as $table => $rows) {
             foreach ($rows as $row) {
-                $this->insert($table, $row);
+                $this->insert($this->tablesPrefix . $table, $row);
             }
         }
 
         return true;
     }
 
-    private function _downKeys()
+    /**
+     * удаление внешних ключей
+     *
+     * @return bool
+     */
+    private function downKeys()
     {
         if (empty($this->foreignKeys)) {
             return true;
         }
 
         foreach ($this->foreignKeys as $key) {
-            $name = $this->_getFkName($key);
-            $this->dropForeignKey($name, $key['from'][0]);
+            $name = $this->getFkName($key);
+            $this->dropForeignKey($name, $this->tablesPrefix . $key['from'][0]);
         }
 
         return true;
     }
 
-    private function _getFkName($key)
+    /**
+     * генерирует имя внешнего ключа
+     *
+     * @param string $key
+     *
+     * @return string
+     */
+    private function getFkName($key)
     {
         $name = implode('_', array_merge($key['from'], $key['to']));
 
         return $name;
     }
 
-    private function _downFields()
+    /**
+     * Удаление полей
+     *
+     * @return bool
+     */
+    private function downFields()
     {
         if (empty($this->fields) || $this->table == null) {
             return true;
         }
 
         foreach (array_keys($this->fields) as $field) {
-            $this->dropColumn($this->table, $field);
+            $this->dropColumn($this->tablesPrefix . $this->table, $field);
         }
 
         return true;
     }
 
-    private function _downTable()
+    /**
+     * удаление таблиц
+     *
+     * @return bool
+     */
+    private function downTable()
     {
         if (empty($this->tables)) {
             return true;
         }
 
         foreach (array_keys($this->tables) as $table) {
-            $this->dropTable($table);
+            $this->dropTable($this->tablesPrefix . $table);
         }
 
         return true;
